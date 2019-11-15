8aa3ef8bc217ce8c36e49676ec8a917bae74ea86
张酉夫 <zhangyoufu@gmail.com>
2019-11-15 11:35:07 +0800

    fix clean workflow

--- .github/clean.py
+++ .github/clean.py
@@ -4,14 +4,13 @@ import requests
 import subprocess
 
 session = requests.Session()
-proc = subprocess.run(['git', 'for-each-ref', '--format=%(refname:short)', 'refs/remotes/origin/??????????????????????????????????'],
+proc = subprocess.run(['git', 'for-each-ref', '--format=%(refname:lstrip=3)', 'refs/remotes/origin/??????????????????????????????????'],
     stdout=subprocess.PIPE,
     check=True,
 )
 to_delete = []
-for ref in proc.stdout.decode().strip().split('\n'):
-    branch = ref.split('/', 1)[-1]
-    product = json.loads(subprocess.run(['git', 'show', f'{ref}:product.json'],
+for branch in proc.stdout.decode().strip().split('\n'):
+    product = json.loads(subprocess.run(['git', 'show', f'refs/remotes/origin/{branch}:product.json'],
         stdout=subprocess.PIPE,
         check=True,
     ).stdout)
@@ -20,7 +19,7 @@ for ref in proc.stdout.decode().strip().split('\n'):
         continue
     if rsp.status_code != 404:
         print(f'[WARNING] {branch} {rsp.status_code} {rsp.reason}')
-    to_delete.append(ref)
+    to_delete.append('refs/heads/'+branch)
     print(f'[INFO] purge {branch}')
 if to_delete:
     subprocess.run(['git', 'push', '--delete', 'origin'] + to_delete, check=True)
--- .github/clean.sh
+++ .github/clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 
+git config --global http.extraheader "Authorization: Basic $(echo -n "x-access-token:${GITHUB_TOKEN}" | base64 --wrap=0)"
 pip3 install -r .github/requirements.txt
 
 exec .github/clean.py
